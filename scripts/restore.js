d4d86fec0240167b1c2e1a8d098a67c828c3c4a2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-01-16 01:24:33 +0800

    fix IDE error with react app

--- scripts/restore.js
+++ scripts/restore.js
@@ -4,3 +4,8 @@ let data = require('../package.json')
 data.homepage = 'https://github.com/beenotung/use-state-proxy#readme'
 let text = JSON.stringify(data, null, 2) + '\n'
 fs.writeFileSync('package.json', text)
+
+data = require('../tsconfig.json')
+data.compilerOptions.jsx = 'react'
+text = JSON.stringify(data, null, 2) + '\n'
+fs.writeFileSync('tsconfig.json', text)
